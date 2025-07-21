78e21e7b8fe1be520e8bbacae4194cd7716949cd
ishidadecol <ogonnotaki@gmail.com>
2025-07-21 14:04:43 -0300

    feat(products): created controller with mock data

---
+++ express-api/src/controllers/productController.js
@@ -0,0 +1,88 @@
+/*WILL USE MOCK DATA WHILE DATABASE IS NOT IMPLEMENTED */
+import Product from "../models/Product";
+
+const products = [
+    new Product("57e95887-dc12-4a11-a801-352e658147d1", 'Laptop', 1999.99, 'High-performance laptop for developers'),
+    new Product("9c5765fc-9504-41e0-a536-7b759da1fe92", 'Smartphone', 899.49, 'Latest model smartphone'),
+    new Product("9cc3ea34-390a-4036-bf49-a27997a4c125", 'Headphones', 129.99, 'Noise-cancelling over-ear headphones'),
+    new Product("23a689c9-999c-43ea-b1e5-c196aae6811a", 'Monitor', 299.00, '27-inch 4K monitor'),
+];
+
+//MARK: GET ALL PRODUCTS
+export const getAllProducts = (req, res) => {
+    //TODO: Fetch all products from DB
+    return res.json(products);
+}
+
+//MARK: GET PRODUCT BY ID
+export const getProductById = (req, res) => {
+    //TODO: Fetch a specific product using its Id
+    const id = req.params.id;
+
+    const product = products.find(p => p.id === id);
+
+    if (!product) {
+        return res.status(404).json({ error: 'Product not found' });
+    }
+
+    res.json(product)
+}
+
+//MARK: CREATE A PRODUCT
+export const createProduct = (req, res) => {
+    //TODO: Create a product in DB
+    const { name, price, description } = req.body;
+
+    if (!name || price === undefined) {
+        return res.status(400).json({ error: 'Name and price are required' });
+    }
+
+    const newProduct = {
+        /*SEQUALIZE WILL LATER GENERATE A UUID WHEN CREATING A PRODUCT
+        I dont feel like installing uuid package rn*/
+        name,
+        price,
+        description
+    };
+
+    products.push(newProduct);
+    res.status(201).json(newProduct);
+}
+
+//MARK: UPDATE A PRODUCT
+export const updateProductById = (req, res) => {
+    //TODO: Update a specific product in DB
+    const id = req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+
+    if (productIndex === -1) {
+        return res.status(404).json({ error: 'Product not found' });
+    }
+
+    const existingProduct = products[productIndex];
+
+    const updatedProduct = {
+        ...existingProduct,
+        name: req.body.name ?? existingProduct.name,
+        price: req.body.price ?? existingProduct.price,
+        description: req.body.description ?? existingProduct.description
+    };
+
+    products[productIndex] = updatedProduct;
+
+    res.json(updatedProduct)
+}
+
+//MARK: DELETE A PRODUCT
+export const deleteProductById = (res, req) => {
+    //TODO: Deleta aspecific product from db
+    const id = req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+
+    if (productIndex === -1) {
+        return res.status(404).json({ error: 'Product not found' });
+    }
+
+    products.splice(productIndex, 1);
+    res.status(204).send();
+}
